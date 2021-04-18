6c6afb58d29186af2bb46a06a04b40c12f6dbd69
Mark Edward Ratio <markedwardratio@gmail.com>
2021-04-18 13:26:22 +0800

    Handle some err in friendships#create

--- controllers/friendshipsController.js
+++ controllers/friendshipsController.js
@@ -49,36 +49,49 @@ exports.create = [
 						req.currentUser.sendFriendRequest(
 							req.body.requesteeId,
 							(err, friendship) => {
-								req.currentUser.friendships.push(friendship._id);
-								req.currentUser.save((err) => {
-									if (err) {
-										friendship.remove((err) => {
-											if (err) {
-												return next(err);
-											}
-										});
-										return next(err);
-									} else {
-										User.findById(req.body.requesteeId).exec(
-											(err, requestee) => {
-												requestee.friendships.push(friendship._id);
-												requestee.save((err) => {
+								if (err) {
+									next(err);
+								} else {
+									req.currentUser.friendships.push(friendship._id);
+									req.currentUser.save((err) => {
+										if (err) {
+											friendship.remove((err) => {
+												if (err) {
+													return next(err);
+												}
+											});
+											return next(err);
+										} else {
+											User.findById(req.body.requesteeId).exec(
+												(err, requestee) => {
 													if (err) {
 														friendship.remove((err) => {
 															if (err) {
 																return next(err);
 															}
 														});
-														return next(err);
+														next(err);
 													} else {
-														// Successful
-														res.status(201).json({ friendship });
+														requestee.friendships.push(friendship._id);
+														requestee.save((err) => {
+															if (err) {
+																friendship.remove((err) => {
+																	if (err) {
+																		return next(err);
+																	}
+																});
+																return next(err);
+															} else {
+																// Successful
+																res.status(201).json({ friendship });
+															}
+														});
 													}
-												});
-											}
-										);
-									}
-								});
+												}
+											);
+										}
+									});
+								}
 							}
 						);
 					}
